add835cf1686e2286cd609692e24549cb28145b1
Klava1 <klavdiya.bolshakova@gmail.com>
2024-04-05 17:47:55 +0100

    Initial commit

---
+++ cheesecake/cheesecake.js
@@ -0,0 +1,39 @@
+const timer = 45;
+let amountTime = 45 * 60;
+const btn = document.querySelector("#btn");
+let timerId;
+const player = document.querySelector("#player");
+
+btn.addEventListener("click", startTimer);
+
+function startTimer() {
+  if (!timerId) {
+    timerId = setInterval(calculateTime, 1000);
+    player.play()
+  }
+}
+
+function calculateTime() {
+  let countdown = document.querySelector("#countdown");
+  let minutes = Math.floor(amountTime / 60);
+  let seconds = amountTime % 60;
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  countdown.textContent = `${minutes}:${seconds}`;
+  amountTime--;
+
+  if (amountTime < 0) {
+    stopTimer(timerId);
+  }
+}
+
+function stopTimer(id) {
+  clearInterval(id);
+  player.pause();
+  timerId = null;
+  amountTime = timer * 60;
+  countdown.textContent = "45:00";
+}
